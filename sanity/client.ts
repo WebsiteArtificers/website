76c89b4682f2a49353db8e44c07c1946bd00046f
Samuel Teigland <stsoccer922@gmail.com>
2023-10-03 14:23:24 -0500

    hopefully fixed sanity issue

--- sanity/client.ts
+++ sanity/client.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET_NAME,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID  || process.env.SANITY_STUDIO_PROJECT_ID || process.env.SANITY_API_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET || process.env.SANITY_API_DATASET,
   apiVersion: '1',
   useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
