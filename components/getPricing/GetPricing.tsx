b4714b59fc209d037bfcb58865340ab02ffb6f15
Samuel Teigland <stsoccer922@gmail.com>
2023-10-03 13:42:27 -0500

    fixed stripe code bug

--- components/getPricing/GetPricing.tsx
+++ components/getPricing/GetPricing.tsx
@@ -93,7 +93,7 @@ export default function GetPricing({
       </section>
     );
 
-  if (products.length)
+  if (products.length === 1)
     return (
       <section className="bg-black">
         <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
