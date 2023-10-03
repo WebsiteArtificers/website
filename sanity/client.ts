2116206b45bcccdc449fa4324ea69d3fbf1d4b50
Samuel Teigland <stsoccer922@gmail.com>
2023-10-03 14:35:56 -0500

    attempting to fix sanity

--- sanity/client.ts
+++ sanity/client.ts
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID  || process.env.SANITY_STUDIO_PROJECT_ID || process.env.SANITY_API_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET || process.env.SANITY_API_DATASET,
   apiVersion: '1',
-  useCdn: false,
+  useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 })
 
