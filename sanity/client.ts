c6f05c4afa0d25e15eeebaac598aa26ec1318580
Samuel Teigland <stsoccer922@gmail.com>
2023-10-03 16:01:04 -0500

    maybe this will work

--- sanity/client.ts
+++ sanity/client.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.SANITY_API_PROJECT_ID,
-  dataset: process.env.SANITY_API_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
   apiVersion: '2023-10-03',
   useCdn: false,
   token: process.env.SANITY_API_READ_TOKEN,
