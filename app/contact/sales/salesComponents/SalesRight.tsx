59e513f11e0d3e4e87eb4acd66672551afb5941f
WebsiteArtificers <samuelteigland@websiteartificers.com>
2023-11-17 11:25:51 -0600

    maybe this will work

--- app/contact/sales/salesComponents/SalesRight.tsx
+++ app/contact/sales/salesComponents/SalesRight.tsx
@@ -32,7 +32,7 @@ export default function SalesRight() {
         }
         
         try {
-            await fetch('/api/submitForm', {
+            await fetch('/api/submit-form', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
