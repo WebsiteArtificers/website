0bbdce1ff12fd5a1beb6426558a021ff3167f874
WebsiteArtificers <samuelteigland@websiteartificers.com>
2023-12-02 11:06:17 -0600

    fixed some of the media query issues with the pricing page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -44,7 +44,10 @@ export default function ThankYouPage() {
             <div className='thankYou__container'>
                 <h1 className='thankYou__header'>Thank you for reaching out</h1>
                 <p className='thankYou__paragraph thankYou__top'>We have received your submission and will be in touch soon.</p>
-                <p className='thankYou__paragraph thankYou__bottom'>You can also reach us by emailing contact@websiteartificers.com or by checking us out on <a className='thankYou__link' href='https://www.linkedin.com/company/website-artificers' target="_blank" rel="noopener noreferrer">LinkedIn</a> and <a className='thankYou__link' href='https://www.facebook.com/websiteartificers' target="_blank" rel="noopener noreferrer">Facebook</a>. For the latest news and guides, <a className='thankYou__link' href='/blog'>check out our blog</a>!</p>
+                <p className='thankYou__paragraph thankYou__bottom'>You can also reach us by emailing contact@websiteartificers.com or by checking us out on
+                <a className='thankYou__link' href='https://www.linkedin.com/company/website-artificers' target="_blank" rel="noopener noreferrer">LinkedIn</a> and
+                <a className='thankYou__link' href='https://www.facebook.com/websiteartificers' target="_blank" rel="noopener noreferrer">Facebook</a>. For the latest news and guides,
+                <a className='thankYou__link' href='/blog'>check out our blog</a>!</p>
                 <div className='thankYou__button--container'>
                     <a href='/contact'>
                         <button className='thankYou__button button__primary'>Contact us</button>
