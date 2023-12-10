c60039b6f4fcae8b0f1244d3954e966919db7b47
WebsiteArtificers <samuelteigland@websiteartificers.com>
2023-12-10 08:02:15 -0600

    added some links to the navigation menu to be able to get to the platforma and frontend pages

--- components/navbar/hamburgerMenu/menus/ProductMenu.tsx
+++ components/navbar/hamburgerMenu/menus/ProductMenu.tsx
@@ -5,15 +5,15 @@ export default function Product() {
         <div className='hamburgerMenu__container'>
             <div className='hamburgerMenu__subcontainer'>
                 <h1 className='hamburgerMenu__subcontainer--header'>Platform</h1>
-                <a className='hamburgerMenu__subcontainer--link'>
+                <a className='hamburgerMenu__subcontainer--link' href='/platform/frontend'>
                     <h2 className='hamburgerMenu__subcontainer--subheader'>Frontend</h2>
                     <p className='hamburgerMenu__subcontainer--description'>Your virtual storefront</p>
                 </a>
-                <a className='hamburgerMenu__subcontainer--link'>
+                <a className='hamburgerMenu__subcontainer--link' href='/platform/data'>
                     <h2 className='hamburgerMenu__subcontainer--subheader'>Data</h2>
                     <p className='hamburgerMenu__subcontainer--description'>Supercharge your business</p>
                 </a>
-                <a className='hamburgerMenu__subcontainer--link'>
+                <a className='hamburgerMenu__subcontainer--link' href='/platform/backend'>
                     <h2 className='hamburgerMenu__subcontainer--subheader'>Backend</h2>
                     <p className='hamburgerMenu__subcontainer--description'>Securely manage your business</p>
                 </a>
--- components/navbar/regularMenu/menus/ProductMenu.tsx
+++ components/navbar/regularMenu/menus/ProductMenu.tsx
@@ -5,15 +5,15 @@ export default function Product() {
         <div className='menu__container'>
             <div className='menu__subcontainer'>
                 <h1 className='menu__subcontainer--header'>Platform</h1>
-                <a className='menu__subcontainer--link'>
+                <a className='menu__subcontainer--link' href='/platform/frontend'>
                     <h2 className='menu__subcontainer--subheader'>Frontend</h2>
                     <p className='menu__subcontainer--description'>Your virtual storefront</p>
                 </a>
-                <a className='menu__subcontainer--link'>
+                <a className='menu__subcontainer--link' href='/platform/data'>
                     <h2 className='menu__subcontainer--subheader'>Data</h2>
                     <p className='menu__subcontainer--description'>Supercharge your business</p>
                 </a>
-                <a className='menu__subcontainer--link'>
+                <a className='menu__subcontainer--link' href='/platform/backend'>
                     <h2 className='menu__subcontainer--subheader'>Backend</h2>
                     <p className='menu__subcontainer--description'>Securely manage your business</p>
                 </a>
