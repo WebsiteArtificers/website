75c35d0d68ea5b433be4ad1ccb20c224fcb98499
WebsiteArtificers <samuelteigland@websiteartificers.com>
2023-12-15 21:31:42 -0500

    added meta information to the terms and privacy pages

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,5 +1,41 @@
 import './privacyPolicy.css'
 
+const meta = {
+  title: 'The Website Artificers: Our Privacy Policy',
+  description: 'Come here to learn more about how we safely manage your privacy and data.'
+  cardImage: '/images/website-artificer-logo.ico',
+  robots: 'follow, index',
+  favicon: '/images/website-artificer-logo.ico',
+  url: process.env.NEXT_PUBLIC_SITE_URL + '/privacy-policy',
+  type: 'website'
+};
+
+export const metadata = {
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_SITE_URL}`),
+  title: meta.title,
+  description: meta.description,
+  cardImage: meta.cardImage,
+  robots: meta.robots,
+  favicon: meta.favicon,
+  url: meta.url,
+  type: meta.type,
+  openGraph: {
+    url: meta.url,
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage,
+    type: meta.type,
+    site_name: meta.title
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@websiteartificers',
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage
+  }
+};
+
 export default function Page() {
     return(
         <div className='privacyPolicy__container'>
--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,5 +1,41 @@
 import './terms.css';
 
+const meta = {
+  title: 'The Website Artificers: Our Terms of Service',
+  description: 'Come here to learn more about our terms of service policies.'
+  cardImage: '/images/website-artificer-logo.ico',
+  robots: 'follow, index',
+  favicon: '/images/website-artificer-logo.ico',
+  url: process.env.NEXT_PUBLIC_SITE_URL + '/terms',
+  type: 'website'
+};
+
+export const metadata = {
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_SITE_URL}`),
+  title: meta.title,
+  description: meta.description,
+  cardImage: meta.cardImage,
+  robots: meta.robots,
+  favicon: meta.favicon,
+  url: meta.url,
+  type: meta.type,
+  openGraph: {
+    url: meta.url,
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage,
+    type: meta.type,
+    site_name: meta.title
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@websiteartificers',
+    title: meta.title,
+    description: meta.description,
+    cardImage: meta.cardImage
+  }
+};
+
 export default function Page() {
     return(
         <div className='terms__container'>
