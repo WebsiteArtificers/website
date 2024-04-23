62cb35154e7af87cce9b558b72f9019648f9e143
WebsiteArtificers <samuelteigland@websiteartificers.com>
2024-04-23 11:48:36 -0500

    added more to the hero section

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -12,7 +12,10 @@ export default function Hero() {
             <motion.p className='hero__paragraph' transition={{ delay: 1 }} animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
                 Your online presence is a crucial part of your business. Let us make your website functional and scalable.
                 <br></br>
-                We offer a variety of plans that will supercharge your website, without paying the cost of a dedicated, full-time, web developer or designer.
+                <br></br>
+                <span>
+                  Each of our plans costs much less than a dedicated, full-time, web developer or designer. A web developer can sometimes cost upwards of $100,000/year, and that doesn't even include benefits. We cost a fraction of that amount while offering the same capabilities.
+                </span>
             </motion.p>
             <div className='hero__button--container'>
                 <a href='/contact'>
