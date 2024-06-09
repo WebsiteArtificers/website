a4e8ba6ca00a14b89e6d410dfd62370918470601
Samuel Teigland <samuelteigland@websiteartificers.com>
2024-06-09 14:25:22 -0500

    made some changes to the pricing page

--- components/getPricing/PricingComparison.tsx
+++ components/getPricing/PricingComparison.tsx
@@ -6,78 +6,40 @@ export default function PricingComparison() {
             <h2 className='pricingComparison__header'>Compare Plans</h2>
             <div className='pricingComparison__subcontainer'>
                     <h2 className='pricingComparison__plan--header top__row--header' data-left-column>Plans</h2>
-<<<<<<< HEAD
-                    <h2 className='pricingComparison__plan--header top__row--header'>Starter</h2>
-=======
->>>>>>> 5be521a (made some changes to the pricing page)
                     <h2 className='pricingComparison__plan--header top__row--header'>Standard</h2>
                     <h2 className='pricingComparison__plan--header top__row--header'>Professional</h2>
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Pricing</h2>
-<<<<<<< HEAD
-                    <h3 className='pricingComparison__plan--subheader'>$400<span>/per month</span></h3>
-                    <h3 className='pricingComparison__plan--subheader'>$700<span>/per month</span></h3>
-                    <h3 className='pricingComparison__plan--subheader'>$1,500<span>/per month</span></h3>
-
-                    <h2 className='pricingComparison__plan--header' data-left-column >Requests at a Time</h2>
-                    <h3 className='pricingComparison__plan--subheader'>1</h3>
-                    <h3 className='pricingComparison__plan--subheader'>1</h3>
-=======
                     <h3 className='pricingComparison__plan--subheader'>$50<span>/per month</span></h3>
                     <h3 className='pricingComparison__plan--subheader'>$100<span>/per month</span></h3>
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Requests at a Time</h2>
                     <h3 className='pricingComparison__plan--subheader'>1</h3>
->>>>>>> 5be521a (made some changes to the pricing page)
                     <h3 className='pricingComparison__plan--subheader'>2</h3>
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Delivery Time</h2>
                     <h3 className='pricingComparison__plan--subheader'>48 <span>hours</span></h3>
-<<<<<<< HEAD
-                    <h3 className='pricingComparison__plan--subheader'>36 <span>hours</span></h3>
                     <h3 className='pricingComparison__plan--subheader'>24 <span>hours</span></h3>
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Max Website Size</h2>
-                    <h3 className='pricingComparison__plan--subheader'>10 <span>pages</span></h3>
-=======
-                    <h3 className='pricingComparison__plan--subheader'>24 <span>hours</span></h3>
-
-                    <h2 className='pricingComparison__plan--header' data-left-column >Max Website Size</h2>
->>>>>>> 5be521a (made some changes to the pricing page)
                     <h3 className='pricingComparison__plan--subheader'>20 <span>pages</span></h3>
                     <h3 className='pricingComparison__plan--subheader'>Unlimited</h3>
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Our Available Services</h2>
                     <h3 className='pricingComparison__plan--subheader'>Web Development, SEO</h3>
-<<<<<<< HEAD
-                    <h3 className='pricingComparison__plan--subheader'>Web Development, Marketing, SEO</h3>
-=======
->>>>>>> 5be521a (made some changes to the pricing page)
                     <h3 className='pricingComparison__plan--subheader'>Web Development, Design, Marketing, SEO, and more</h3>
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Pause Feature</h2>
                     <h3 className='pricingComparison__plan--subheader'>✓</h3>
                     <h3 className='pricingComparison__plan--subheader'>✓</h3>
-<<<<<<< HEAD
-                    <h3 className='pricingComparison__plan--subheader'>✓</h3>
-=======
->>>>>>> 5be521a (made some changes to the pricing page)
 
                     <h2 className='pricingComparison__plan--header' data-left-column >Easy Credit Card payments</h2>
                     <h3 className='pricingComparison__plan--subheader'>✓</h3>
                     <h3 className='pricingComparison__plan--subheader'>✓</h3>
-<<<<<<< HEAD
-                    <h3 className='pricingComparison__plan--subheader'>✓</h3>
-=======
->>>>>>> 5be521a (made some changes to the pricing page)
  
                     <h2 className='pricingComparison__plan--header' data-left-column >Cancel Anytime</h2>
                     <h3 className='pricingComparison__plan--subheader'>✓</h3>
                     <h3 className='pricingComparison__plan--subheader'>✓</h3>
-<<<<<<< HEAD
-                    <h3 className='pricingComparison__plan--subheader'>✓</h3>
-=======
->>>>>>> 5be521a (made some changes to the pricing page)
             </div>
         </div>
     )
