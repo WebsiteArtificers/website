9e7697e36ecbc8dfce3ec2bda69710d5dc2c37b9
Samuel Teigland <samuelteigland@websiteartificers.com>
2024-06-13 11:27:07 -0500

    fixed up the thank you page a bit

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -44,16 +44,13 @@ export default function ThankYouPage() {
             <div className='thankYou__container'>
                 <h1 className='thankYou__header'>Thank you for reaching out</h1>
                 <p className='thankYou__paragraph thankYou__top'>We have received your submission and will be in touch soon.</p>
-                <p className='thankYou__paragraph thankYou__bottom'>You can also reach us by emailing contact@websiteartificers.com or by checking us out on
-                <a className='thankYou__link' href='https://www.linkedin.com/company/website-artificers' target="_blank" rel="noopener noreferrer">LinkedIn</a> and
-                <a className='thankYou__link' href='https://www.facebook.com/websiteartificers' target="_blank" rel="noopener noreferrer">Facebook</a>. For the latest news and guides,
-                <a className='thankYou__link' href='/blog'>check out our blog</a>!</p>
+                <p className='thankYou__paragraph thankYou__bottom'>You can also reach us by emailing contact@websiteartificers.com or learn more about us by checking us out on <a className='thankYou__link' href='https://www.youtube.com/@SamTeigland' target="_blank" rel="noopener noreferrer">YouTube</a>.</p>
                 <div className='thankYou__button--container'>
                     <a href='/contact'>
                         <button className='thankYou__button button__primary'>Contact us</button>
                     </a>
-                    <a href='/about'>
-                        <button className='thankYou__button button__tertiary'>More about us</button>
+                    <a href='/'>
+                        <button className='thankYou__button button__tertiary'>Return Home</button>
                     </a>
                 </div>
             </div>
